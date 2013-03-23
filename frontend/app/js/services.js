015da9725618855c9cbe12588105446a916b16fc
Matthew Dawson <matthew@mjdsystems.ca>
2013-03-23 17:38:32 -0400

    Add initial pieces to deal with authentication.
    The system now sort of works for authentication.  It doesn't remember urls,
    but otherwise it works just fine.

--- frontend/app/js/services.js
+++ frontend/app/js/services.js
@@ -6,4 +6,25 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+	value('version', '0.1').
+	service('authentication', function($http) {
+		this.verify_session = function() {
+			return $http.get("api/ses/verify_session").then(function(res) {
+				return res.data;
+			});
+		}
+	}).
+	constant('authenticationResolveVerifier', function($q, $window, authentication) {
+		var q = $q.defer();
+
+		authentication.verify_session().then(function(res) {
+			if(res.hasToken === true) {
+				q.resolve();
+			} else {
+				$window.location.replace(res.redirect_url);
+				q.reject();
+			}
+		});
+
+		return q.promise;
+	});
--- frontend/test/unit/servicesSpec.js
+++ frontend/test/unit/servicesSpec.js
@@ -3,12 +3,117 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+	beforeEach(module('myApp.services'));
 
+	var $httpBackend;
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+	beforeEach(inject(function($injector) {
+		$httpBackend = $injector.get('$httpBackend');
+	}));
+
+	describe('version', function() {
+		it('should return current version', inject(function(version) {
+			expect(version).toEqual('0.1');
+		}));
+	});
+	describe('authentication', function() {
+		var authServer;
+
+		beforeEach(inject(function($injector) {
+			authServer = {
+				hasToken: false
+			};
+
+			// backend definition common for all tests
+			$httpBackend.when('GET', 'api/ses/verify_session').respond(function() {
+				if(authServer.hasToken == true) {
+					return [200, {'hasToken': true}];
+				} else {
+					return [200, {'hasToken': false, 'redirect_url': 'the_url'}];
+				}
+			});
+		}));
+
+		it('verifies user initially not logged in gets url.', inject(function(authentication, $rootScope) {
+			$httpBackend.expectGET('api/ses/verify_session');
+
+			authentication.verify_session().then(function(result) {
+				expect(result).toEqual({'hasToken': false, 'redirect_url': 'the_url'});
+			});
+
+			$httpBackend.flush();
+			$rootScope.$digest();
+		}));
+
+		it('verifies user logged in is fine.', inject(function(authentication, $rootScope) {
+			authServer.hasToken = true;
+			$httpBackend.expectGET('api/ses/verify_session');
+
+			authentication.verify_session().then(function(result) {
+				expect(result).toEqual({'hasToken': true});
+			});
+
+			$httpBackend.flush();
+			$rootScope.$digest();
+		}));
+
+	});
+
+	describe('Authentication Resolver Verifier', function() {
+		var authServer;
+		var $window;
+		var authenticationResolveVerifier;
+
+		beforeEach(inject(function($injector) {
+			authServer = {
+				hasToken: false
+			};
+
+			// backend definition common for all tests
+			$httpBackend.when('GET', 'api/ses/verify_session').respond(function() {
+				if(authServer.hasToken == true) {
+					return [200, {'hasToken': true}];
+				} else {
+					return [200, {'hasToken': false, 'redirect_url': 'http://login_url/'}];
+				}
+			});
+
+			$window = {
+				location: {
+					href: "http://localhost",
+					replace: function(url) {
+						this.href = url
+					}
+				}
+			};
+
+			authenticationResolveVerifier = $injector.get('authenticationResolveVerifier');
+		}));
+
+		it('verifies redirection when user not logged in..', inject(function(authentication, $rootScope, $q) {
+			$httpBackend.expectGET('api/ses/verify_session');
+
+			authenticationResolveVerifier($q, $window, authentication).then(function() {
+				this.fail(Error("Should not have a sucessful resolve!"));
+			}.bind(this), function() {
+				expect($window.location.href).toEqual('http://login_url/');
+			});
+
+			$httpBackend.flush();
+			$rootScope.$digest();
+		}));
+
+		it('verifies user logged in is fine.', inject(function(authentication, $rootScope, $q) {
+			authServer.hasToken = true;
+			$httpBackend.expectGET('api/ses/verify_session');
+
+			authenticationResolveVerifier($q, $window, authentication).then(function() {
+			}, function() {
+				this.fail(Error("Should have verified auth!"));
+			}.bind(this));
+
+			$httpBackend.flush();
+			$rootScope.$digest();
+		}));
+	});
 });
