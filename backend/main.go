a8b1f21516f08638a7abc6f1f27d87275740bad9
Matthew Dawson <matthew@mjdsystems.ca>
2013-04-11 23:01:00 -0400

    Move back to using an http server for the application.
    Due to using websockets, fastcgi is no longer an option.  Instead move back
    to using a full HTTP server.  Nginx can be used to proxy requests as needed,
    if a frontend web server is necessary.

--- backend/main.go
+++ backend/main.go
@@ -3,17 +3,10 @@ package main
 import (
 	"flag"
 
-	"net"
 	"net/http"
-	"net/http/fcgi"
-
-	"os"
-
-	"syscall"
 )
 
 var prefix = flag.String("prefix", "", "The prefix to use to identify this installation.")
-var socket = flag.String("socket", "/tmp/eimbu.socket", "The fcgi socket to listen on.")
 
 func main() {
 	if !flag.Parsed() {
@@ -29,16 +22,5 @@ func main() {
 
 	regexHandler.Handle("/ses/{request}", oauthFlowHandler{})
 
-	var socket = *socket
-
-	os.Remove(socket)
-
-	oldUmask := syscall.Umask(000)
-	l, err := net.Listen("unix", socket)
-	syscall.Umask(oldUmask)
-	if err != nil {
-		panic(err)
-	}
-
-	panic(fcgi.Serve(l, http.DefaultServeMux))
+	panic(http.ListenAndServe("localhost:8080", http.DefaultServeMux))
 }
